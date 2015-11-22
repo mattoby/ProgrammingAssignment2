50193e9c731fa44149a90b86ae560ec52b0ea374
mattoby <mattoby@gmail.com>
2015-11-21 22:27:29 -0500

    first commit of assignment 2 by Matt O

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This source file includes a set of two functions, one of which holds a matrix
+## (and the matrix's inverse), and the other of which determines the inverse of
+## the matrix when passed the output of the first function (which is also a
+## function). For coursera R programming, assignment 2
+#
+## Matt Oberhardt, 11/21/2015
 
-## Write a short comment describing this function
+
+## this makes a function, which holds information about a matrix. The matrix's
+## inverse, once calculated, is cached in the outer function, i.e., the
+## environment of the inner function (via lexical scoping). The contents of the
+## matrix can be set as such:
+# usage:
+# CM <- makeCacheMatrix()
+# CM$get() # outputs matrix
+# CM$set(<<define matrix to be set here>>)
+# CM$getinv() # outputs inverse of the matrix
+# CM$setinv() # this can be used to set the inverse..
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(newinv) inv <<- newinv
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a CM object (produced by makeCacheMatrix) and sets the
+## inverse of the matrix.
+# usage:
+# inv <- cacheSolve(CM) # inv is the inverse of the matrix
+# all.equal(CM$getinv(),inv) # this shows that the inverse was set in CM
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
